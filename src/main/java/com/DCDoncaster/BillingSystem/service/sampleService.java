d0cc73eac1150b6147e83e8a81867be5795760c8
David Clarke <david26th@gmail.com>
2022-09-14 18:56:44 +0100

    Added Text message Chase for outstanding balances

--- src/main/java/com/DCDoncaster/BillingSystem/service/sampleService.java
+++ src/main/java/com/DCDoncaster/BillingSystem/service/sampleService.java
@@ -10,9 +10,19 @@ import com.DCDoncaster.BillingSystem.repository.sampleRepository;
 import java.util.List;
 import java.util.Optional;
 import java.util.Objects;
+import java.net.URI;
+import java.math.BigDecimal;
+
+import com.twilio.Twilio;
+import com.twilio.converter.Promoter;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
 
 @Service
 public class sampleService {
+    public static final String ACCOUNT_SID = "ACc9c84b76cf25af228f8dc7dfd7660046";
+    public static final String AUTH_TOKEN = " 2bb3f3f50efd379c94b0fe468bf2e0fc";
     public static double unitRate = 0.2855;
     @Autowired
     sampleRepository samplerepository;
@@ -37,6 +47,21 @@ public class sampleService {
             existingdata.setOutstandingbalance(newCharge + existingBalance);
             existingdata.setLastreading(dataupdate.getLastreading());
         };
+
+
+
+
+            Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+            Message message = Message.creator(
+                            new com.twilio.type.PhoneNumber("+447784897307"), //this could be set to the phone number of the customer in the database to be more dynamic
+                            "MGb8c8a0f88021746132fa467d2a859247",
+                            "Your new balance on your account is £" + existingdata.getOutstandingbalance())
+                    .create();
+            System.out.println("Banana Pancakes");
+            System.out.println(message.getSid());
+
+
+
         return samplerepository.save(existingdata);
 }
 //Delete Account but only if the outstanding balance is set at 0 - if below 0 shoudl trigger a method to issue a refund for the customer.
